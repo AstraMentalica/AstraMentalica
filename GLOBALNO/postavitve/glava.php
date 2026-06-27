c31a48c1be7ef9a1b51a05923de83e5a28f2ec38
AstraMentalica <familydiamont432@gmail.com>
2026-06-27 13:10:17 +0200

    premetan frontend

--- GLOBALNO/postavitve/glava.php
+++
@@ -1,8 +0,0 @@
-<?php //GLOBALNO/gradniki/glava.php
-if (!defined('SISTEM_VARNOST')) die(); ?>
-<header class="glava">
-    <div class="glava-vsebina">
-        <div class="logo"><a href="/">🔮 Astra</a></div>
-        <button class="meni-gumb" id="meniGumb">☰</button>
-    </div>
-</header>
